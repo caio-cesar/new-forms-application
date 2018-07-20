37a45b3e8c53691748df4d60aa9dbe449dcaa327
caio-cesar <caiocesouza@gmail.com>
2018-07-20 20:47:34 -0300

    New message

---
+++ src/main/java/com/mydomain/newformsapplication/NewFormsApplication.java
@@ -0,0 +1,12 @@
+package com.mydomain.newformsapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFormsApplication.class, args);
+	}
+}
